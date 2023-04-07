4582ce09df500e9a8c200b2dc20bfaefc9095f08
Ilyass Krichi <mc.mono20@gmail.com>
2023-04-07 16:59:57 +0000

    initial commit

---
+++ src/Controller/IParkController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IParkController extends AbstractController
+{
+    #[Route('/i/park', name: 'app_i_park')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/IParkController.php',
+        ]);
+    }
+}
