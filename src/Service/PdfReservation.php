c841287216cad6046aa39eca0f8a8aecb6d749a8
Mohamed-KBIBECH <mohamedkbibech2002@gmail.com>
2023-05-24 16:41:03 +0100

    modified:   src/Service/PdfReservation.php

---
+++ src/Service/PdfReservation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+class PdfService
+{
+    private $domPdf;
+    public function __construct(){
+        $this->domPdf=new DomPdf();
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont','Garamond');
+        $this->domPdf->setOptions($pdfOptions);
+        
+    }
+
+    public function showPdf($html){
+        $this->domPdf->loadHtml($html);
+        $this->domPdf->render();
+        $this->domPdf->stream('details.pdf', ['Attachment' => false]);
+    }
+
+    public function generateBinaryPdf($html){
+        $this->domPdf->loadHtml($html);
+        $this->domPdf->render();
+        $this->domPdf->output();
+    }
+    
+}
